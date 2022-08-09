a17fe5547944dc728e591452fad7966fa8bb4ee6
aidenspro <aidenspro@gmail.com>
2022-08-09 17:23:13 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <img src={karathrow} alt="loading..." />
+      <img src={"https://imgur.com/dht3eiQ"} alt="loading..." />
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
