23fb7c16af5e72d98bd7087a3bbdcf1f00a5e63c
aidenspro <aidenspro@gmail.com>
2022-08-09 17:14:54 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import karathrow from '/gifs/karathrow.gif'
+
 import "./style.css";
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
+      <img src={karathrow} alt="loading..." />
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
