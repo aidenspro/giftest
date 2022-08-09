6f0767d216758da0d6a71f20b756c57102b32dcc
aidenspro <aidenspro@gmail.com>
2022-08-09 18:10:01 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import karathrow from './gifs/karathrow.gif'
+import React from 'react';
+import karathrow from './gifs/karathrow.gif';
 
-import "./style.css";
+import './style.css';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <img src={"https://imgur.com/dht3eiQ"} alt="loading..." />
+      <img src={'https://imgur.com/7f8lC4W'} alt="loading..." />
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
